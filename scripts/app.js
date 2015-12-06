9241ef5a3b8ede0d16203014608cb726f95a69e1
mmmfang <mae.fang@gmail.com>
2015-12-06 18:01:37 -0500

    modals and sliders work but sporadically

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,16 @@
+//SLIDER WILL INITIALIZE UPON ENTERING SITE
     $(document).ready(function(){
       $('.slider').slider({full_width: true});
     });
         
-        
+//MODAL TRIGGER  
+   $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+       
+
+
 // // Pause slider
 // $('.slider').slider('pause');
 // // Start slider
