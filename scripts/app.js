6f22438c0736d9b8f7868f84df685b3b257e0eb0
mmmfang <mae.fang@gmail.com>
2015-12-06 17:38:58 -0500

    made images into a carousel

--- scripts/app.js
+++ scripts/app.js
@@ -2,3 +2,12 @@
       $('.slider').slider({full_width: true});
     });
         
+        
+// // Pause slider
+// $('.slider').slider('pause');
+// // Start slider
+// $('.slider').slider('start');
+// // Next slide
+// $('.slider').slider('next');
+// // Previous slide
+// $('.slider').slider('prev');
